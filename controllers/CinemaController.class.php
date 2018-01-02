ce1d14dca4c68d9a3eb4a2adeea63495ee8534a8
Unknown <mihai95gabriel@gmail.com>
2018-01-02 12:51:04 +0100

    (Ri)Commit
    Non avevo fatto lo stage dei file...

--- controllers/CinemaController.class.php
+++ controllers/CinemaController.class.php
@@ -2,108 +2,111 @@
 use Slim\Http\Request;
 use Slim\Http\Response;
 
-class CinemaController
-{
-	/**
-	 * @desc This method provides the registration html page
-	 * @link /home
-	 * @method GET
-	 * @param Request $request
-	 * @param Response $response
-	 * @param array $args
-	 * @return \Slim\Http\Response
-	 */
-	function home(Request $request, Response $response, $args)
+	class CinemaController
 	{
-
-try{
-
-		$film = new Film();
-		$visualizzato['visualizzato']=true;
-		$data['film']=$film->select($visualizzato);
-		$id_scheda['id_scheda']=$data['film'][0]['id_scheda'];
-		$scheda=new Scheda();
-		$data['scheda']=$scheda->select($id_scheda);
-		// var_dump($data);exit();
-		$table = (new HomeView(null,$data));
-		$page = new Page();
-		$page->addView("content",$table);
-		return $response->write($page->render());
-	
-	}
-	/**
-	 * @desc This method provides the registration html page
-	 * @link /admin
-	 * @method GET
-	 * @param Request $request
-	 * @param Response $response
-	 * @param array $args
-	 * @return \Slim\Http\Response
-	 */
-	function admin(Request $request, Response $response, $args)
-	{
-		try{
-			$feed = new FeedRssController();
-			$data=$feed->FeedRss();
-			$table = (new FeedRssView(null,$data));
-			$page = new Page();
-			$page->addView("content",$table);
-			return $response->write($page->render());
-		}
-		catch(Exception $e )
+		/**
+		* @desc This method provides the registration html page
+		* @link /home
+		* @method GET
+		* @param Request $request
+		* @param Response $response
+		* @param array $args
+		* @return \Slim\Http\Response
+		*/
+		function home(Request $request, Response $response, $args)
 		{
-			echo $e->getMessage();
-		}	
-	}
-	/**
-	 * @desc This method provides the registration html page
-	 * @link /save_film
-	 * @method 
-	 * @param Request $request
-	 * @param Response $response
-	 * @param array $args
-	 * @return \Slim\Http\Response
-	 */
-	function save_film(Request $request, Response $response, $args)
-	{
-		$url =$_POST["url"];
-		$attori =$_POST["attori"];
-		$regia =$_POST["regia"];
-		$descrizione =$_POST["descrizione"];
-		$titolo =$_POST["titolo"];
-		$data_inizio=$_POST["data_inizio"];
-		$data_fine=$_POST["data_fine"];
-		$vowels = array(" ","'");
-		$titolourl=  str_replace($vowels, '', $titolo);
-		// var_dump($titolourl);exit();
-		$img = "image/".$titolourl.".jpg";
-		file_put_contents($img, file_get_contents($url));
-		//$insert_film['']=$img;
-		$insert_scheda['attori']=$attori;
-		$insert_scheda['regia']=$regia;
-		// var_dump($insert_scheda);exit();
-		$insert_film['visualizzato']=TRUE;
-		$insert_film['immagine']=$img;
-		$insert_film['titolo']= $titolo;
-		$insert_film['descrizione']=$descrizione;
-		$insert_film['data_inizio']=$data_inizio;
-		$insert_film['data_fine']=$data_fine;
-		var_dump($data_inizio);
-		$film = new Film();
-		$scheda=new Scheda();	
-		$scheda->insert($insert_scheda);
+			try {
+				$film = new Film();
+				$visualizzato['visualizzato']=true;
+				$data['film']=$film->select($visualizzato);
+				$id_scheda['id_scheda']=$data['film'][0]['id_scheda'];
+				$scheda=new Scheda();
+				$data['scheda']=$scheda->select($id_scheda);
+				// var_dump($data);exit();
+				$table = (new HomeView(null,$data));
+				$page = new Page();
+				$page->addView("content",$table);
+				return $response->write($page->render());	
+			}
+			catch(Exception $e)
+			{	
+				echo $e ->getMessage();
+			}
+		}
+		/**
+		 * @desc This method provides the registration html page
+		 * @link /admin
+		 * @method GET
+		 * @param Request $request
+		 * @param Response $response
+		 * @param array $args
+		 * @return \Slim\Http\Response
+		 */
+
+		function admin(Request $request, Response $response, $args)
+		{
+			try{
+				$feed = new FeedRssController();
+				$data=$feed->FeedRss();
+				$table = (new FeedRssView(null,$data));
+				$page = new Page();
+				$page->addView("content",$table);
+				return $response->write($page->render());
+			}
+			catch(Exception $e )
+			{
+				echo $e->getMessage();
+			}	
+		}
+		/**
+		 * @desc This method provides the registration html page
+		 * @link /save_film
+		 * @method 
+		 * @param Request $request
+		 * @param Response $response
+		 * @param array $args
+		 * @return \Slim\Http\Response
+		 */
+		function save_film(Request $request, Response $response, $args)
+		{
+			$url =$_POST["url"];
+			$attori =$_POST["attori"];
+			$regia =$_POST["regia"];
+			$descrizione =$_POST["descrizione"];
+			$titolo =$_POST["titolo"];
+			$data_inizio=$_POST["data_inizio"];
+			$data_fine=$_POST["data_fine"];
+			$vowels = array(" ","'");
+			$titolourl=  str_replace($vowels, '', $titolo);
+			// var_dump($titolourl);exit();
+			$img = "image/".$titolourl.".jpg";
+			file_put_contents($img, file_get_contents($url));
+			//$insert_film['']=$img;
+			$insert_scheda['attori']=$attori;
+			$insert_scheda['regia']=$regia;
+			// var_dump($insert_scheda);exit();
+			$insert_film['visualizzato']=TRUE;
+			$insert_film['immagine']=$img;
+			$insert_film['titolo']= $titolo;
+			$insert_film['descrizione']=$descrizione;
+			$insert_film['data_inizio']=$data_inizio;
+			$insert_film['data_fine']=$data_fine;
+			var_dump($data_inizio);
+			$film = new Film();
+			$scheda=new Scheda();	
+			$scheda->insert($insert_scheda);
 
 
-		$id=$scheda->get_last_id();
-		$insert_film['id_scheda']=$id[0]['id_scheda'];
-		$film->unvisible_last_id();
-		
-		try {
-			$film->insert($insert_film);
-		}
-		catch(Exception $e )
-	    {
-			echo $e->getMessage();
+			$id=$scheda->get_last_id();
+			$insert_film['id_scheda']=$id[0]['id_scheda'];
+			$film->unvisible_last_id();
+			
+			try {
+				$film->insert($insert_film);
+			}
+			catch(Exception $e )
+			{
+				echo $e->getMessage();
+			}
 		}
 	}
-}
